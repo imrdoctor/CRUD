a948e43efbc0c2ac81644e7bc6f4cd1b3d39cc9b
imrdoctor <mhamedfekry0@gmail.com>
2024-06-06 23:41:09 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var prodactNameInput = document.getElementById("prodactName");
-var prodactPricenput = document.getElementById("prodactPrice");
+var prodactPriceInput = document.getElementById("prodactPrice");
 var prodactCategoryInput = document.getElementById("category");
 var prodactDescInput = document.getElementById("desc");
 var prodactImgInput = document.getElementById("prodactImg");
@@ -15,9 +15,9 @@ if(localStorage.getItem("products") == null){
 }
 function addProdact(){
     var prodact = {
-        image:'images/gg.png',
+        image:`images/${prodactImgInput.files[0].name}`,
         code:prodactNameInput.value,
-        price:prodactPricenput.value,
+        price:prodactPriceInput.value,
         category:prodactCategoryInput.value,
         desc:prodactDescInput.value,
     }
@@ -29,7 +29,7 @@ function addProdact(){
 }
 function clearForm(){
     prodactNameInput.value = null
-    prodactPricenput.value = null
+    prodactPriceInput.value = null
     prodactCategoryInput.value = null
     prodactDescInput.value = null
     prodactImgInput.value = null
@@ -37,17 +37,17 @@ function clearForm(){
 function displayProducts(){
     var cartona = ``
     for(var i=0;i<prodactContainer.length;i++){
-        cartona+=`<div class="col-md-3">
-        <div class="product  rounded">
-            <img class="w-100" src="images/data-analysis.png">
+        cartona += `<div class="col-md-3">
+        <div class="product rounded">
+            <img class="w-100" src="${prodactContainer[i].image}">
             <div class="descreption p-3">
-                <h2 class="h4 mt-3"><span class="">Name : </span>${prodactContainer[i].code}</h2>
-                <h3 class="h5"><span class="">Price :</span>${prodactContainer[i].price}</h3>
-                <h3 class="h5"><span class="">Category :</span>${prodactContainer[i].category}</h3>
-                <p class="h5 mb-3"><span class="">Descreption :</span>${prodactContainer[i].desc}</p>
+                <h2 class="h4 mt-3"><span class="fw-bolder">Name : </span>${prodactContainer[i].code}</h2>
+                <h3 class="h5"><span class="fw-bolder">Price :</span>${prodactContainer[i].price}</h3>
+                <h3 class="h5"><span class="fw-bolder">Category :</span>${prodactContainer[i].category}</h3>
+                <p class="h5 mb-3"><span class="fw-bolder">Description :</span>${prodactContainer[i].desc}</p>
                 <button onclick="editProdact(${i})" class="btn btn-outline-warning d-block w-100 mb-2">Edit <i class="fa-solid fa-pen-alt"></i></button>
                 <button onclick="deletePordact(${i})" class="btn btn-outline-danger d-block w-100 mb-2">Delete <i class="fa-solid fa-trash-alt"></i></button>
-                </div>
+            </div>
         </div>
     </div>`;
     }
@@ -65,31 +65,43 @@ function serchProducts(){
     var cartona = ``;
     for(var i = 0;i<prodactContainer.length;i++){
         if(prodactContainer[i].code.toLowerCase().includes(term.toLowerCase()) == true ){
-            cartona+=`<div class="col-md-3">
-            <div class="product border border-2 border-primary rounded">
-                <img class="w-100" src="images/data-analysis.png">
+            cartona += `<div class="col-md-3">
+            <div class="product rounded">
+                <img class="w-100" src="${prodactContainer[i].image}">
                 <div class="descreption p-3">
                     <h2 class="h4 mt-3"><span class="fw-bolder">Name : </span>${prodactContainer[i].code}</h2>
                     <h3 class="h5"><span class="fw-bolder">Price :</span>${prodactContainer[i].price}</h3>
                     <h3 class="h5"><span class="fw-bolder">Category :</span>${prodactContainer[i].category}</h3>
-                    <p class="h5 mb-3"><span class="fw-bolder">Descreption :</span>${prodactContainer[i].desc}</p>
+                    <p class="h5 mb-3"><span class="fw-bolder">Description :</span>${prodactContainer[i].desc}</p>
                     <button onclick="editProdact(${i})" class="btn btn-outline-warning d-block w-100 mb-2">Edit <i class="fa-solid fa-pen-alt"></i></button>
                     <button onclick="deletePordact(${i})" class="btn btn-outline-danger d-block w-100 mb-2">Delete <i class="fa-solid fa-trash-alt"></i></button>
-                    </div>
+                </div>
             </div>
-        </div>`
+        </div>`;
     }
 }
     document.getElementById("rowData").innerHTML = cartona;
 }
-
-function editProdact(){
+var updateindex;
+function editProdact(editindex){
     console.log('hi');
     addBtn.classList.add("d-none")
     updatebtn.classList.remove("d-none")
+    prodactNameInput.value = prodactContainer[editindex].code;
+    prodactPriceInput.value = prodactContainer[editindex].price;
+    prodactCategoryInput.value = prodactContainer[editindex].category;
+    prodactDescInput.value = prodactContainer[editindex].desc;
+    // prodactImgInput.value = prodactContainer[editindex].img;
+    updateindex = editindex;
 }
 function updateProdact(){
     addBtn.classList.remove("d-none")
     updatebtn.classList.add("d-none")
-        // var code = document.getElementById("code").value;
+    prodactContainer[updateindex].code = prodactNameInput.value;
+    prodactContainer[updateindex].price = prodactPriceInput.value;
+    prodactContainer[updateindex].category = prodactCategoryInput.value;
+    prodactContainer[updateindex].desc = prodactDescInput.value;
+    displayProducts()
+    localStorage.setItem('products',JSON.stringify(prodactContainer));
+    clearForm()
 }
